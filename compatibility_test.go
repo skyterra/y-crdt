6e3c4192b2732b82c8fe902070b25bd302d468c7
Qinghui Yao <sky_terra@126.com>
2025-06-17 15:33:56 +0800

    test(compatibility): add test case for map set operations
    Verify compatibility of map set operations between Go implementation and Yjs by:
    - Constructing a map in Go and comparing with expected JSON output
    - Applying Yjs v1 update and validating the resulting map

--- compatibility_test.go
+++ compatibility_test.go
@@ -1,6 +1,7 @@
 package y_crdt
 
 import (
+	"encoding/json"
 	"testing"
 )
 
@@ -56,4 +57,44 @@ func TestTextInsertDelete(t *testing.T) {
 		t.Errorf("expected abhi, got %s", ytext.ToString())
 	}
 	t.Logf("after apply update, ytext is %s", ytext.ToString())
+}
+
+func TestMapSet(t *testing.T) {
+	//  Generated via:
+	//     ```js
+	//        const doc = new Y.Doc()
+	//        const x = doc.getMap('test')
+	//        x.set('k1', 'v1')
+	//        x.set('k2', 'v2')
+	//        const payload_v1 = Y.encodeStateAsUpdate(doc)
+	//        console.log(payload_v1);
+	//        const payload_v2 = Y.encodeStateAsUpdateV2(doc)
+	//        console.log(payload_v2);
+	//     ```
+
+	// construct doc by golang and check to see if the result is the same as the expected.
+	doc := NewDoc("guid", false, nil, nil, false)
+	x := doc.GetMap("test").(*YMap)
+	doc.Transact(func(trans *Transaction) {
+		x.Set("k1", "v1")
+		x.Set("k2", "v2")
+	}, nil)
+
+	content, err := json.Marshal(x.ToJson())
+	t.Logf("construct by golang, x is %s, err is %v", content, err)
+
+	// apply the update(v1) and check to see if the result is the same as the expected.
+	var payload = []byte{
+		1, 2, 241, 204, 241, 209, 1, 0, 40, 1, 4, 116, 101, 115, 116, 2, 107, 49, 1, 119, 2, 118,
+		49, 40, 1, 4, 116, 101, 115, 116, 2, 107, 50, 1, 119, 2, 118, 50, 0,
+	}
+	doc = NewDoc("guid", false, nil, nil, false)
+	doc.Transact(func(trans *Transaction) {
+		ApplyUpdate(doc, payload, nil)
+	}, nil)
+
+	content, err = json.Marshal(doc.GetMap("test").ToJson())
+	t.Logf("after apply update, x is %s, err is %v", content, err)
+
+	// decoder v2 not support yet.
 }
